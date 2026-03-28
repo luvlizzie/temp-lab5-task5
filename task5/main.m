c6f37b62a61475604a3b6e1070dd68be27a349a8
luvlizzie <kudinovaea2006@yandex.by>
2026-03-28 18:44:19 +0300

    Initial Commit

---
+++ task5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  task5
+//
+//  Created by Елизавета on 28.03.26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
